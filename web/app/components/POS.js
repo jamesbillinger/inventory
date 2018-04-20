745b1f06df1417c1cd9bab6cde33696bc95c4cc2
Jim Billinger <jimbill3@hotmail.com>
2018-04-20 13:34:11 -0500

    commit

--- web/app/components/POS.js
+++ web/app/components/POS.js
@@ -4,9 +4,19 @@ import RaisedButton from 'material-ui/RaisedButton';
 export default class Inventory extends Component {
   render() {
     return (
-      <div style={{flex:'1 1 auto', backgroundColor:'white', borderRadius:'6px', padding:'10px'}}>
-        <div>POS!</div>
-        <RaisedButton label="POS button"  />
+      <div style={{flex:'1 1 auto', display:'grid', gridTemplateColumns:'1fr auto', gridTemplateRows:'auto 1fr auto'}}>
+        <div style={{gridColumn:'1 / 2', gridRow:'1'}}>
+          top
+        </div>
+        <div style={{gridColumn:'1', gridRow:'2'}}>
+          left middle
+        </div>
+        <div style={{gridColumn:'2', gridRow:'2'}}>
+          right middle
+        </div>
+        <div style={{gridColumn:'1 /2', gridRow:'3'}}>
+          bottom
+        </div>
       </div>
     )
   }
