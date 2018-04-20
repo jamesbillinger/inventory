cc5fe8e8c597687f194afe6cbd54ba0f3b946723
Jim Billinger <jimbill3@hotmail.com>
2018-04-20 13:31:05 -0500

    commit

--- web/app/components/inventoryFilter.js
+++ web/app/components/inventoryFilter.js
@@ -4,7 +4,7 @@ import RaisedButton from 'material-ui/RaisedButton';
 export default class InventoryFilter extends Component {
   render() {
     return (
-      <div style={{flex:'1 1 auto', backgroundColor:'white', padding:'15px 0 15px 15px'borderRadius:'6px', width:'230px'}}>
+      <div style={{flex:'1 1 auto', backgroundColor:'white', padding:'15px 0 15px 15px', borderRadius:'6px', width:'230px'}}>
         <div>Inventory Filter</div>
         <RaisedButton label="filter button"  />
       </div>
