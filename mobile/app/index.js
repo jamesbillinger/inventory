807c71a0d70261a2b1bd1ba9c8fe3fa85486a08b
gordo <hamster.doom@gmail.com>
2019-02-12 08:17:58 -0600

    test to make sure git was pushed to correctly with previous commit

--- mobile/app/index.js
+++ mobile/app/index.js
@@ -4,9 +4,9 @@ import React, { Component } from 'react';
 export default class Index extends Component {
   render() {
     return (
-      <View style={{flex:1, jusifyContent:'center'}}>
+      <View style={{flex:1, justifyContent:'center'}}>
         <View style={{flex:1, flexDirection:'row', justifyContent:'center'}}>
-          <Text>Hello</Text>
+          <Text>Hello World</Text>
         </View>
       </View>
     )
